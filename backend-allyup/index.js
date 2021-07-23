03f71e4e65d42cae58eeeb3af9f96805a62b2f2b
calee14 <caplee1000@gmail.com>
2021-07-23 11:35:26 -0700

    add signup func

--- backend-allyup/index.js
+++ backend-allyup/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const authMiddleware = require("./auth-middleware");
+const firebase = require("./firebase/index")
 
 const app = express();
 app.use(cors());
 
 app.use("/", authMiddleware);
+
+app.use("/api/signup", (req, res) => {
+    const { email, password } = req.body
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        // Signed in 
+        var user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ..
+    });
+})
 
 app.listen(4000, () => console.log("The server is running at PORT 4000"));
