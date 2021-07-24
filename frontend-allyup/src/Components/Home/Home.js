9f0316ff9249c4a5b0d817fe6483c3857bda9c29
Tom <imatomster@gmail.com>
2021-07-23 18:20:15 -0700

    home page

---
+++ frontend-allyup/src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'Home.css'
+
+export default function Home() {
+    return (
+        <div className="home-container">
+            <div className="header">
+                Hello I am header
+            </div>
+        </div>
+    )
+}
