61b348fbc8706d95f1a10bc088b4afe461152467
Warder3 <gothit877@gmail.com>
2021-07-24 17:57:18 -0700

    categories working

--- frontend-allyup/src/Components/Home/Hero/Hero.js
+++ frontend-allyup/src/Components/Home/Hero/Hero.js
@@ -1,39 +1,10 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect} from 'react'
 import './Hero.css'
 import NewPost from './NewPost/NewPost'
-import axios from 'axios'
+
 import Post from '../Post/Post'
 
-export default function Hero() {
-
-    const [userData, setUserData] = useState({})
-    const [posts, setPosts] = useState([]) 
-    const [isShelter, setIsShelter] = useState(false);  
-
-    useEffect(() => {        
-        axios.get('http://localhost:4000/api/user')
-          .then((response) => {  
-            const res = response.data                                       
-            setUserData(res)      
-            setIsShelter(res.isShelter)
-            console.log(res)
-            console.log(isShelter)
-          },                    
-          (error) => {
-            console.log(error);
-        });
-        
-        axios.get('http://localhost:4000/api/posts')
-          .then((response) => {  
-            const res = response.data                                       
-            setPosts(res)
-            console.log(res)
-          },                    
-          (error) => {
-            console.log(error);
-        });
-
-    }, []);
+export default function Hero({posts, isShelter, userData}) {
 
     return (
         <div className="hero-container">
